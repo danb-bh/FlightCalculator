c7073c773b9f1d7f45ecf042085ddf4b7b8c2015
Daniel Burnett <osw@users.noreply.github.com>
2020-08-14 23:13:45 +0100

    refactor to create types

--- src/main.py
+++ src/main.py
@@ -1,54 +1,81 @@
 import unittest
 import pandas as pd
 
+path_to_all_coordinates_csv = '../data/airport-codes_csv.csv'
+path_to_flights = '../data/flights.txt'
 
-def get_locations(data):
+
+class Location:
+
+    def __init__(self, code: str):
+        self.code = code.strip().upper()
+        self.name = ''
+        self.latitude = 0.0
+        self.longitude = 0.0
+
+    def __eq__(self, other):
+        return self.code == other.code
+
+    def __hash__(self):
+        return hash(self.code)
+
+    def __str__(self):
+        return self.code
+
+
+class Trip:
+
+    def __init__(self, depart: Location, arrive: Location, repetitions=1):
+        self.depart = depart
+        self.arrive = arrive
+        self.repetitions = repetitions
+        self.distance = 0.0
+
+    def __eq__(self, other):
+        return self.arrive == other.arrive and self.depart == other.depart
+
+    def __hash__(self):
+        return hash((self.depart, self.arrive))
+
+    def __str__(self):
+        return f'{self.depart},{self.arrive},{self.repetitions}'
+
+
+def get_trips(data):
     current = None
+
+    def create_trip(datum):
+        elements = datum.split(',')
+        depart = Location(elements[0])
+        arrive = Location(elements[1])
+        repetitions = 1 if len(elements) == 2 else int(elements[2])
+        return Trip(depart, arrive, repetitions)
 
     for line in data:
 
         trimmed = line.strip()
         if trimmed == '': continue
 
-        elements = trimmed.split(',')
-        whitespace_removed = []
-        for element in elements:
-            whitespace_removed.append(element.strip())
-
         previous = current
-        current = ','.join(whitespace_removed).upper()
+        current = trimmed
         if previous is None: continue
 
         if previous[0] != '-' and current[0] != '-':
-            yield previous
+            yield create_trip(previous)
 
-    yield current
-
-
-def get_full_names(data):
-    found = set()
-    for line in data:
-        elements = line.split(',')[:2]
-        for element in elements:
-            if len(element) > 3 and element not in found:
-                found.add(element)
-                yield element
+    yield create_trip(current)
 
 
-def determine_trips(data):
+def combine_trips(data):
     trips = {}
 
-    for line in data:
-        elements = line.split(',')
-        key = elements[0] + ',' + elements[1]
-        times = 1 if len(elements) == 2 else int(elements[2])
-
-        if key not in trips:
-            trips[key] = times
+    for trip in data:
+        if trip not in trips:
+            trips[trip] = trip
         else:
-            trips[key] += times
+            trips[trip].repetitions += trip.repetitions
 
-    return trips
+    return trips.values()
 
 
 def get_coordinates(all_coordinates, airport):
@@ -66,21 +93,19 @@ def get_coordinates(all_coordinates, airport):
 class Tests(unittest.TestCase):
 
     def test_get_coordinates(self):
-        path = '../data/airport-codes_csv.csv'
-        all_coordinates = pd.read_csv(path)
+        all_coordinates = pd.read_csv(path_to_all_coordinates_csv)
         self.assertEqual([-0.461941, 51.4706], get_coordinates(all_coordinates, 'LHR'))
         self.assertEqual([13.2877, 52.5597], get_coordinates(all_coordinates, 'TXL'))
 
     def test_get_coordinates_bad_airport_code(self):
-        path = '../data/airport-codes_csv.csv'
-        all_coordinates = pd.read_csv(path)
+        all_coordinates = pd.read_csv(path_to_all_coordinates_csv)
 
         with self.assertRaises(Exception) as ex:
             self.assertEqual([0, 0], get_coordinates(all_coordinates, 'XXX'))
 
         self.assertEqual('Unable to find airport code XXX', str(ex.exception))
 
-    def test_determine_trips(self):
+    def test_combine_trips(self):
         data = [
             'LHR,AGP,2',
             'AGP,LHR,2',
@@ -90,28 +115,20 @@ class Tests(unittest.TestCase):
             'TXL,LGW',
             'DOH,LHR'
         ]
-        expected = {
-            'LHR,AGP': 2,
-            'AGP,LHR': 2,
-            'LGW,TXL': 2,
-            'TXL,LGW': 2,
-            'DOH,LHR': 1
-        }
-        actual = determine_trips(data)
+        expected = [
+            'LHR,AGP,2',
+            'AGP,LHR,2',
+            'LGW,TXL,2',
+            'TXL,LGW,2',
+            'DOH,LHR,1'
+        ]
+
+        all_trips = get_trips(data)
+        actual = [str(trip) for trip in combine_trips(all_trips)]
+
         self.assertEqual(expected, actual)
 
-    def test_get_full_names(self):
-        data = [
-            'LONDON,MALAGA,2',
-            'MALAGA,LONDON,2',
-            'LHR,TXL',
-            'TXL,LHR']
-
-        expected = ['LONDON', 'MALAGA']
-        actual = list(get_full_names(data))
-        self.assertEqual(expected, actual)
-
-    def test_get_locations(self):
+    def test_get_trips(self):
         data = [
             'from,to,repetitions',
             '---',
@@ -127,22 +144,26 @@ class Tests(unittest.TestCase):
         expected = [
             'LHR,AGP,2',
             'AGP,LHR,2',
-            'LHR,TXL',
-            'TXL,LHR']
+            'LHR,TXL,1',
+            'TXL,LHR,1']
 
-        actual = list(get_locations(data))
+        actual = [str(trip) for trip in get_trips(data)]
         self.assertEqual(expected, actual)
 
     def test_display_example(self):
         self.skipTest('')
-        path = '../data/flights.txt'
-        with open(path) as file:
+
+        with open(path_to_flights) as file:
             data = file.readlines()
 
-        data = get_locations(data)
-        trips = determine_trips(data)
+        all_coordinates = pd.read_csv(path_to_all_coordinates_csv)
+
+        data = get_trips(data)
+        trips = combine_trips(data)
         for trip in trips:
-            print(f'{trip}: {trips[trip]}')
+            airports = trip.split(',')
+            depart = get_coordinates(all_coordinates, airports[0])
+            arrive = get_coordinates(all_coordinates, airports[1])
 
 
 if __name__ == '__main__':
